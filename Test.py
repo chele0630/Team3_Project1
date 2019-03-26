372cbf7129888987e05c139301eba05823c7f571
Ka-Ri Dywayne Walker <dywayne.walker@gmail.com>
2019-03-25 19:24:02 -0500

    Working on Test
    THis is a test

---
+++ Test.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 1.0.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import os
+import pandas as pd
+import numpy as np
+
+
+Hey Guys hows it going this is our Production Testing
